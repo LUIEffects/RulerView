0c0c6e6945779900e24f041a4db32132ee21c2b2
taitsun <taitsun@tencent.com>
2019-10-09 11:51:25 +0800

    没什么卵用的提交，整理一diudiu代码

--- seekview/src/main/java/com/yangshipin/seekview/SeekViewActivity.java
+++ seekview/src/main/java/com/yangshipin/seekview/SeekViewActivity.java
@@ -2,6 +2,7 @@ package com.yangshipin.seekview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListAdapter;
 import android.widget.TextView;
 
@@ -20,21 +21,16 @@ public class SeekViewActivity extends AppCompatActivity {
         /**
          * 准备数据
          */
-        SeekViewDataObj seekViewDataObj = new SeekViewDataObj();
-        List<SeekViewDataObj.ScaleMsgObj> list = new ArrayList<>();
+        final SeekViewDataObj seekViewDataObj = new SeekViewDataObj();
+        final List<SeekViewDataObj.ScaleMsgObj> list = new ArrayList<>();
         mTv = (TextView) findViewById(R.id.mTv);
-//        list.add(new SeekViewDataObj.ScaleMsgObj(6, "1:00", "舒克和贝塔"));
-//        list.add(new SeekViewDataObj.ScaleMsgObj(13, "2:10", "猫和老鼠"));
-//        list.add(new SeekViewDataObj.ScaleMsgObj(20, "3:10", "海绵宝宝"));
-//        list.add(new SeekViewDataObj.ScaleMsgObj(31, "5:00", "早上新闻"));
-//        list.add(new SeekViewDataObj.ScaleMsgObj(43, "7:00", "洗脑新闻"));
-//        list.add(new SeekViewDataObj.ScaleMsgObj(49, "8:00", "破冰行动"));
-//        list.add(new SeekViewDataObj.ScaleMsgObj(82, "13:30", "午休结束"));
-//        list.add(new SeekViewDataObj.ScaleMsgObj(93, "15:20", "下午茶"));
-//        list.add(new SeekViewDataObj.ScaleMsgObj(114, "18:50", "晚间新闻"));
-//        list.add(new SeekViewDataObj.ScaleMsgObj(127, "22:00", "走进科学"));
-//        list.add(new SeekViewDataObj.ScaleMsgObj(139, "23:00", "午夜凶铃"));
-
+        findViewById(R.id.mBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                seekView.setVisibility(seekView.getVisibility() == View.VISIBLE ? View.INVISIBLE : View.VISIBLE);
+                seekView.refreshData(seekViewDataObj);
+            }
+        });
 
         //2019 10 9
         list.add(new SeekViewDataObj.ScaleMsgObj(1570554000, "1:00", "舒克和贝塔"));
